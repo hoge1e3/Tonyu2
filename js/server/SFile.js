c7133f2e636af19045af53b8e1c88c014603f3d6
hoge1e3 <hoge1e3@gmail.com>
2014-03-07 14:24:16 +0900

    node.js server

--- js/server/SFile.js
+++ js/server/SFile.js
@@ -1,6 +1,7 @@
 var fs=require("fs");
+var SEP="/";
 function SFile(path) {
-	this._path=path;
+	this._path=fs.realpathSync(path).replace(/\\/g,SEP);
 }
 function extend(dst,src) {
 	for (var i in src) dst[i]=src[i];
@@ -11,14 +12,21 @@ function endsWith(str,postfix) {
 function startsWith(str,prefix) {
     return str.substring(0, prefix.length)===prefix;
 }
-var SEP="/";
 extend(SFile.prototype,{
 	text:function () {
 		if (arguments.length==0) {
-			return fs.readFileSync(this._path);
+			return fs.readFileSync(this._path, {encoding:"utf8"});
 		} else {
 			fs.writeFileSync(this._path, arguments[0]);
 		}
+	},
+	path: function () { return this._path; },
+	name: function () {
+		var p=this._path;
+		if (endsWith(p,SEP)) {
+			p=p.substring(0,p.length-1);
+		}
+		return p.split(SEP).pop();
 	},
 	each:function (it) {
 		if (!this.isDir()) throw this+" cannot each. not a dir.";
@@ -29,6 +37,15 @@ extend(SFile.prototype,{
 			it(f);
 		});
 	},
+	parent: function () {
+		return this.up();
+	},
+	recursive:function (fun) {
+        this.each(function (f) {
+            if (f.isDir()) f.recursive(fun);
+            else fun(f);
+        });
+    },
 	toString: function () {
 		return this._path;
 	},
@@ -36,12 +53,28 @@ extend(SFile.prototype,{
 		if (!this.isDir()) throw this+" cannot rel. not a dir.";
 		return new SFile(this._path+(endsWith(this._path,SEP)?"":SEP)+n);
 	},
+	relPath: function (base) {
+		//console.log("relpath "+this+" - "+base);
+		if ( this.equals(base)) {
+			return ".";
+		}
+		if (this.parent() == null)
+			throw this + " is not in " + base;
+		return this.parent().relPath(base) + SEP + this.name();
+	},
+	equals: function (f) {
+		return (f instanceof SFile) && f.path()==this.path();
+	},
+	lastModified: function () {
+		return this.stat().mtime.getTime();
+	},
 	up: function () {
 		var p=this._path;
 		while (p.length>0) {
 			p=p.substring(0,p.length-1);
 			if (endsWith(p,SEP)) break;
 		}
+		if (p=="") return null;
 		return new SFile(p);
 	},
 	isDir: function () {
--- js/server/main.js
+++ js/server/main.js
@@ -2,19 +2,88 @@ var express = require('express');
 var app = express.createServer();
 var SFile = require("./SFile");
 
-app.get('/File2LS', function(req, res){
+var fsHome=SFile.get("fs");
+app.configure(function () {
+    app.use(express.static(__dirname+"/../../" ,{maxAge:864000000}));
+    app.use(express.staticCache());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
+app.get('/File2LS', function(req, resp){
 	//res.send(req.query.base);
-	res.send( SFile.get("Libs.txt").text() );
+	resp.setHeader("Content-Type", "text/plain;charset=utf8");
+	var base=req.query.base;
+    var basef;
+    if (base==null) {
+        basef=fsHome;
+        base="/";
+    } else {
+        basef=fsHome.rel(base.substring(1));
+    }
+    var data={},dirs={};
+    basef.recursive(function (f) {
+        var p=toLSPath(basef, f);
+        data[p]= f.text();
+        dirs[f.parent().path()]=1;
+    });
+    for (var dirn in dirs) {
+    	var d=SFile.get(dirn);
+        var pp=toLSPath(basef, d);
+        var ddata={};
+        d.each(function (f) {
+            ddata[f.name()+ (f.isDir()?"/":"")]=f.lastModified();
+        });
+        var pdatas=JSON.stringify(ddata);
+        data[pp]=pdatas;
+    }
+    res={base:base, data:data};
+	resp.send( res);
 });
+function toLSPath(/*SFile*/ base, /*SFile*/ child) {
+    var p=child.relPath(base);
+    p=p.replace(/\\/g, "/");
+    if (startsWith(p,"./")) {
+        p=p.substring(2);
+    }
+    if (child.isDir() && !endsWith(p,"/")) {
+        p=p+"/";
+    }
+    if (p==("./")) {
+        p="";
+    }
+    return p;
+}
+function endsWith(str,postfix) {
+    return str.substring(str.length-postfix.length)===postfix;
+}
+function startsWith(str,prefix) {
+    return str.substring(0, prefix.length)===prefix;
+}
 app.post('/LS2File', function(req, res){
-	var base=req.body.base;
-
+    console.log(req.body);
+    //console.log(req);
+	var s=req.body.json;
+	var m=JSON.parse(s);
+    var bases = m.base;
+    bases=bases.substring(1);
+    var data=m.data;
+    for (var fn in data) {
+        var text=data[fn];
+        var path=bases+fn;
+        if (endsWith(path,"/")) continue;
+        var dst=fromLSPath(path);
+        console.log(path+"->"+dst);
+        if (dst.isDir()) continue;
+        dst.text(text);
+    }
 });
+function fromLSPath(path) {
+    if (startsWith(path,"/")) path=path.substring(1);
+    return fsHome.rel(path);
+}
 
 
-app.use(express.static(__dirname+"/../../" ,{maxAge:864000000}));
-app.use(express.staticCache());
-app.use(express.bodyParser());
+//app.use(express.bodyDecoder());
 var http = require('http');
 var server = http.createServer(app);
 server.listen(3000);
