e53eb4cc65007563e435f953d8c9cb7df2f47ced
hoge1e3 <hoge1e3@gmail.com>
2014-10-07 13:17:06 +0900

    somewhat

--- js/gen/project_concat.js
+++ js/gen/project_concat.js
@@ -1,4 +1,4 @@
-// Created at Tue Oct 07 2014 10:03:15 GMT+0900 (東京 (標準時))
+// Created at Tue Oct 07 2014 13:16:31 GMT+0900 (東京 (標準時))
 (function () {
 	var R={};
 	R.def=function (reqs,func,type) {
