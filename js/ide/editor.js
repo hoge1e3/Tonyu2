11fa0718608601f0ca7f5926fbfa2a0e110e08bf
hoge1e3 <hoge1e3@gmail.com>
2014-11-05 11:18:53 +0900

    assert login on project

--- js/ide/editor.js
+++ js/ide/editor.js
@@ -443,5 +443,17 @@ $(function () {
     FM.onMenuStart=save;
     curPrj.compileKernel();
     SplashScreen.hide();
+    if (curPrj.getBlobInfos().length>0) {
+        var ld=UI("div",{title:"ログイン"},["div","このプロジェクトを正常に動作させるにはログインが必要です"]);
+        Auth.assertLogin({
+            showLoginLink:function (u) {
+                ld.append(UI("a",{href:u,target:"login",style:"font-size: 20px;"},"ログインする"));
+            },
+            success:function () {
+                ld.dialog("close");
+            }
+        });
+        ld.dialog({modal:true});
+    }
 });
 });
