e4a36ed282b92dcaa611b098308b072ce6ffc012
hoge1e3 <hoge1e3@gmail.com>
2015-04-28 11:40:08 +0900

    build

--- js/runtime/TonyuLib.js
+++ js/runtime/TonyuLib.js
@@ -500,7 +500,7 @@ return Tonyu=function () {
             globals:globals, classes:classes, setGlobal:setGlobal, getGlobal:getGlobal, getClass:getClass,
             timeout:timeout,asyncResult:asyncResult,bindFunc:bindFunc,not_a_tonyu_object:not_a_tonyu_object,
             hasKey:hasKey,invokeMethod:invokeMethod, callFunc:callFunc,checkNonNull:checkNonNull,
-            VERSION:1430107917750,//EMBED_VERSION
+            VERSION:1430188772935,//EMBED_VERSION
             A:A};
 }();
 });
