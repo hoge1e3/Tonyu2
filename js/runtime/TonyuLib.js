2b7c6be041f445bfd8b75d59d1779c03bc7ded5d
hoge1e3 <hoge1e3@gmail.com>
2015-05-12 14:47:08 +0900

    mysterious bug 2

--- js/runtime/TonyuLib.js
+++ js/runtime/TonyuLib.js
@@ -515,7 +515,7 @@ return Tonyu=function () {
             globals:globals, classes:classes, setGlobal:setGlobal, getGlobal:getGlobal, getClass:getClass,
             timeout:timeout,asyncResult:asyncResult,bindFunc:bindFunc,not_a_tonyu_object:not_a_tonyu_object,
             hasKey:hasKey,invokeMethod:invokeMethod, callFunc:callFunc,checkNonNull:checkNonNull,
-            VERSION:1431405545816,//EMBED_VERSION
+            VERSION:1431409450350,//EMBED_VERSION
             A:A};
 }();
 });
