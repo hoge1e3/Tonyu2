9a492d1dcd01e626f756903d4799ab6b482acc3f
hoge1e3 <hoge1e3@gmail.com>
2015-06-14 17:12:20 +0900

    split error

--- js/runtime/TonyuLib.js
+++ js/runtime/TonyuLib.js
@@ -528,7 +528,7 @@ return Tonyu=function () {
             globals:globals, classes:classes, setGlobal:setGlobal, getGlobal:getGlobal, getClass:getClass,
             timeout:timeout,asyncResult:asyncResult,bindFunc:bindFunc,not_a_tonyu_object:not_a_tonyu_object,
             hasKey:hasKey,invokeMethod:invokeMethod, callFunc:callFunc,checkNonNull:checkNonNull,
-            VERSION:1434267662434,//EMBED_VERSION
+            VERSION:1434269499786,//EMBED_VERSION
             A:A};
 }();
 });
