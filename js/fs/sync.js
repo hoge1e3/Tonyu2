11458f79720276fa5ac8df47e0f6dd2bb44d327a
hoge1e3 <hoge1e3@gmail.com>
2015-10-07 18:21:26 +0900

    rollback sync

--- js/fs/sync.js
+++ js/fs/sync.js
@@ -1,45 +1,60 @@
-define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
-        function (FS,sh,/*rf,*/WebSite,SFile,A) {
+define(["FS","Shell","requestFragment","WebSite"],function (FS,sh,rf,WebSite) {
     var Sync={};
     sh.sync=function () {
-        // sync options:o      local=remote=cwd
-        // sync dir:s|file options:o local=remote=dir
-        // sync local:s|file remote:s|file options:o
+        // sync options:o onend:f     local=remote=cwd
+        // sync dir:s|file options:o onend:f  local=remote=dir
+        // sync local:s|file remote:s|file options:o onend:f
         var local,remote,options,onend=function(){};
         var i=0;
-        if (typeof arguments[i]=="string" || SFile.is(arguments[i])) {
+        if (typeof arguments[i]=="string" || isFile(arguments[i])) {
             local=sh.resolve(arguments[i], true);
             i++;
-            if (typeof arguments[i]=="string" || SFile.is(arguments[i])) {
+            if (typeof arguments[i]=="string" || isFile(arguments[i])) {
                 remote=sh.resolve(arguments[i], false);
                 i++;
             }
         }
         if (typeof arguments[i]=="object") { options=arguments[i]; i++;}
+        if (typeof arguments[i]=="function") { onend=arguments[i]; i++;}
         if (!local) remote=local=sh.cwd;
+        if (options && options.onend) options.onend=promptAfter(options.onend);
         if (!remote) remote=local;
-        sh.echo("sync args=",local,remote,options);
-        return Sync.sync(local,remote,options);
+        sh.echo("sync args=",local,remote,options,onend);
+        Sync.sync(local,remote,options,promptAfter(onend));
+        return sh.ASYNC;
+        function promptAfter(f) {
+            return function () {
+                //alert("pro");
+                if (f) f.apply({},arguments);
+                sh.prompt();
+            };
+        }
     };
-    Sync.NOT_LOGGED_IN="Not logged in.";
+    function isFile(v) {
+        return v && v.isDir;
+    }
     Sync.sync=function () {
-        // sync dir:file options:o local=remote=dir
-        // sync local:file remote:file options:o
-        var local,remote,options;
+        // sync dir:file options:o onend:f  local=remote=dir
+        // sync local:file remote:file options:o onend:f
+        var local,remote,options,onend;
         var i=0;
-        if (SFile.is(arguments[i])) {
+        if (isFile(arguments[i])) {
             local=arguments[i];
             i++;
-            if (SFile.is(arguments[i])) {
+            if (isFile(arguments[i])) {
                 remote=arguments[i];
                 i++;
             }
         }
         if (typeof arguments[i]=="object") { options=arguments[i]; i++;}
+        if (typeof arguments[i]=="function") { onend=arguments[i]; i++;}
+
         if (!local) throw "Sync.sync: Local dir must be specified as file object";
         if (!remote) remote=local;
         if (!options) options={};
+        if (!onend && options.onend) onend=options.onend;
         if (options.test) options.v=1;
+        n0();
         var uploads={},downloads=[],visited={};
         function status(name, param) {
             sh.echo("Status: "+name+" param:",param);
@@ -52,28 +67,26 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
                 options.onerror.apply(this, arguments);
             }
         }
-        var req={base:remote.path(),excludes:JSON.stringify(options.excludes)};
-        status("getDirInfo", req);
-        return $.ajax({
-            type:"get",
-            url:A(WebSite.url.getDirInfo),
-            data:req
-        }).then(function n1(info) {
+        function n0() {
+            var req={base:remote.path(),excludes:JSON.stringify(options.excludes)};
+            status("getDirInfo", req);
+          //TODO: urlchange!
+            $.ajax({
+                type:"get",
+                url:WebSite.serverTop+"/getDirInfo",
+                data:req,
+                success:n1,
+                error:onError
+            });
+        }
+        function n1(info) {
             //info=JSON.parse(info);
-            var d;
             if (options.v) sh.echo("getDirInfo",info);
-            if (info.NOT_LOGGED_IN) {
-                d = new $.Deferred;
-                setTimeout(function(){
-                  d.reject(Sync.NOT_LOGGED_IN);
-                }, 0);
-                return d.promise();
-            }
             var base=local;//FS.get(info.base);
             var data=info.data;
             for (var rel in data) {
                 var file=base.rel(rel);
-                var lcm=file.exists({includeTrashed:true}) ? file.metaInfo() : null;
+                var lcm=file.metaInfo();
                 var rmm=data[rel];
                 cmp(file,rel,lcm,rmm);
             }
@@ -87,14 +100,19 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
                 sh.echo("uploads:",uploads);
                 sh.echo("downloads:",downloads);
             }
+
             var req={base:remote.path(),paths:JSON.stringify(downloads)};
-            status("getFiles", req);
-            return $.ajax({
+            status("File2LSSync", req);
+          //TODO: urlchange!
+            $.ajax({
                 type:"post",
-                url:A(WebSite.url.getFiles),
-                data:req
+                url:WebSite.serverTop+"/File2LSSync",
+                data:req,
+                success:n2,
+                error:onError
             });
-        }).then(function n2(dlData) {
+        }
+        function n2(dlData) {
             sh.echo("dlData=",dlData);
             //dlData=JSON.parse(dlData);
             if (options.v) sh.echo("dlData:",dlData);
@@ -113,40 +131,42 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
                 dlf.metaInfo(d);
             }
             var req={base:remote.path(),data:JSON.stringify(uploads)};
-            req.pathInfo=A(WebSite.url.putFiles);
-            status("putFiles", req);
-            return $.ajax({  // TODO:requestFragment
+            req.pathInfo="/LS2FileSync";//TODO: urlchange!
+            status("LS2FileSync", req);
+          //TODO: urlchange!
+            rf.ajax({
                 type:"post",
-                url:req.pathInfo,
-                data:req
+                url:WebSite.serverTop+"/LS2FileSync",
+                data:req,
+                success:n3,
+                error:onError
             });
-        }).then(function n3(res){
-            if (options.v) sh.echo("putFiles res=",res);
+        }
+        function n3(res){
+            if (options.v) sh.echo("LS2FileSync res=",res);
             var upds=[];
             for (var i in uploads) upds.push(i);
-            return res={msg:res,uploads:upds,downloads: downloads};
-        });
+            res={msg:res,uploads:upds,downloads: downloads};
+            //if (options.v) sh.echo("onend",onend);
+            if (typeof onend=="function") onend(res);
+        }
         function cmp(f,rel,lcm,rmm) {
-            if (options.v) console.log("compare",f.path(), rel,lcm,rmm);
             if (visited[rel]) return ;
             visited[rel]=1;
-            if (rmm && (!lcm || lcm.lastUpdate<rmm.lastUpdate-5000)) {
+            if (rmm && (!lcm || lcm.lastUpdate<rmm.lastUpdate)) {
                 downloads.push(rel);
                 if (options.v)
-                    sh.echo((!lcm?"New ":"")+
-                            "mtime(l-r) "+( (lcm?lcm.lastUpdate:0)-rmm.lastUpdate)+
-                            " Download "+f+
+                    sh.echo((!lcm?"New":"")+
+                            "Download "+f+
                             " trash="+!!rmm.trashed);
-            } else if (lcm && (!rmm || lcm.lastUpdate>rmm.lastUpdate+5000)) {
-                var o={};
-                if (f.exists()) o.text=f.text();
+            } else if (lcm && (!rmm || lcm.lastUpdate>rmm.lastUpdate)) {
+                var o={text:f.text()};
                 var m=f.metaInfo();
                 for (var i in m) o[i]=m[i];
                 uploads[rel]=o;
                 if (options.v)
                     sh.echo((!rmm?"New":"")+
-                            "mtime(l-r) "+(lcm.lastUpdate-(rmm?rmm.lastUpdate:0))+
-                            " Upload "+f+
+                            "Upload "+f+
                             " trash="+!!lcm.trashed);
             }
 
@@ -155,10 +175,19 @@ define(["FS","Shell",/*"requestFragment",*/"WebSite","SFile","assert"],
     sh.rsh=function () {
         var a=[];
         for (var i=0; i<arguments.length; i++) a[i]=arguments[i];
-        return $.ajax({
-            url:A(WebSite.url.rsh),
+      //TODO: urlchange!
+        $.ajax({
+            url:WebSite.serverTop+"/rsh",
             data:{args:JSON.stringify(a)},
+            success:function (r) {
+                sh.echo(r);
+            },error:function (req,e,mesg) {
+                sh.err(mesg);
+            },complete:function (){
+                sh.prompt();
+            }
         });
+        return sh.ASYNC;
     };
     return Sync;
 });
