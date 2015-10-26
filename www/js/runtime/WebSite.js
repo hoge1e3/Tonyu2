8cb96f005e712719248543441bca136e3f1b89df
hoge1e3 <hoge1e3@gmail.com>
2015-10-26 14:23:43 +0900

    Kernel move / build

--- www/js/runtime/WebSite.js
+++ www/js/runtime/WebSite.js
@@ -87,15 +87,16 @@ define([], function () {
         getFiles:WebSite.serverTop+"/File2LSSync",
         putFiles:WebSite.serverTop+"/LS2FileSync"
     };
+    window.WebSite.cwd=process.cwd().replace(/\\/g,"/").replace(/\/?$/,"/");
     if (window.WebSite.isNW) {
         if (process.env.TONYU_HOME) {
             window.WebSite.tonyuHome=process.env.TONYU_HOME.replace(/\\/g,"/").replace(/\/?$/,"/");
         } else {
-            window.WebSite.tonyuHome=process.cwd().replace(/\\/g,"/").replace(/\/$/,"")+"/fs/Tonyu/";
+            window.WebSite.tonyuHome=window.WebSite.cwd+"fs/Tonyu/";
         }
         window.WebSite.logdir="/var/log/Tonyu/";
     }
-    window.WebSite.kernelDir=window.WebSite.tonyuHome+"Kernel/";
-    window.WebSite.compiledKernel=window.WebSite.top+"/../fs/Tonyu/Kernel/js/concat.js"
+    window.WebSite.kernelDir=window.WebSite.cwd+"www/Kernel/";
+    window.WebSite.compiledKernel=window.WebSite.top+"/Kernel/js/concat.js"
     return window.WebSite;
 });
