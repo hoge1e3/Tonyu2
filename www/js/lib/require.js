3974af2ac9ee7bc731a699837fae458a2514413b
hoge1e3 <hoge1e3@gmail.com>
2015-11-11 10:08:39 +0900

    requireNoCache

--- www/js/lib/require.js
+++ www/js/lib/require.js
@@ -1869,7 +1869,8 @@ var requirejs, require, define;
                 node.addEventListener('error', context.onScriptError, false);
             }
             node.src = url;
-            if (typeof window=="object" && window.location.href.match(/^http/)) {
+            if (typeof window=="object" && window.location.href.match(/^http/) &&
+                    window.WebSite && window.WebSite.requireNoCache) {
                 node.src += "?"+Math.random();
             }
 
