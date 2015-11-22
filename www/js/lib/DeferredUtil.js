3ed071e0831d681da40a8d098588daa3be6fb738
hoge1e3 <hoge1e3@gmail.com>
2015-11-22 16:36:50 +0900

    doc update

--- www/js/lib/DeferredUtil.js
+++ www/js/lib/DeferredUtil.js
@@ -22,6 +22,36 @@ define([], function () {
                         return DU.throwPromise(e);
                     }
                 };
+            },
+            each: function (set,f) {
+                if (set instanceof Array) {
+                    return DU.loop(function (i) {
+                        if (i>=set.length) return DU.brk();
+                        return $.when(f(set[i])).then(function () {
+                            return i+1;
+                        });
+                    },0);
+                } else {
+                    var objs=[];
+                    for (var i in set) {
+                        objs.push({k:i,v:set[i]});
+                    }
+                    return DU.each(objs,function (e) {
+                        f(e.k, e.v);
+                    });
+                }
+            },
+            loop: function (f,r) {
+                DU.directPromise(r).then(DU.throwF(function () {
+                    var r=f.apply(this,arguments);
+                    if (r.DU_BRK) return r.res;
+                    return $.when(r).then(function (r) {
+                        return DU.loop(f,r);
+                    });
+                }));
+            },
+            brk: function (res) {
+                return {DU_BRK:true,res:res};
             }
     };
     return DU;
