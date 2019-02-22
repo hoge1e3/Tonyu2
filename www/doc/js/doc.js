7fd89cf71d67b9c43b1816bd76009ca5423ff09f
hoge1e3 <hoge1e3@gmail.com>
2019-02-22 11:28:48 +0900

    doc prevent open external on nw.js

--- www/doc/js/doc.js
+++ www/doc/js/doc.js
@@ -6,6 +6,15 @@ $(function () {
             url=url+".html";
             $(this).attr("href",url);
         }
+        if (url && url.match(/^http/) && typeof process!=="undefined") {
+            $(this).attr("href","javascript:;");
+            var appended;
+            $(this).click(function () {
+                if (appended) return;
+                $(this).after("("+ url +")");
+                appended=true;
+            });
+        }
     });
     if (location.href.match(/tonyuedit.appspot.com/)) {
       location.href="http://www.tonyu.jp/project/pages/e/doc/";
