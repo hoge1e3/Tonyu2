95b7a34a509512a2fab116ded796ff9462f616da
hoge1e3 <hoge1e3@gmail.com>
2019-05-12 20:00:37 +0900

    nwjs-update

---
+++ update-nwjs.pl
@@ -0,0 +1,55 @@
+
+# debugDir: hack icon of nw.exe and save as Tonyu2.exe
+$debugDir=$ARGV[0];
+# nonDebugDir: hack icon of nw.exe and save as Player2.exe
+$nonDebugDir=$ARGV[1];
+
+
+@baseFiles=qw(
+d3dcompiler_47.dll
+ffmpeg.dll
+icudtl.dat
+libEGL.dll
+libGLESv2.dll
+locales/*
+natives_blob.bin
+node.dll
+nw.dll
+nw_100_percent.pak
+nw_200_percent.pak
+nw_elf.dll
+resources.pak
+v8_context_snapshot.bin 
+swiftshader/libGLESv2.dll
+swiftshader/libEGL.dll
+);
+&copyDebug;
+&copyNonDebug;
+
+sub copyDebug {
+   my $dst=".";
+   my $src=$debugDir;
+   my @files=@baseFiles;
+   push @files, "Tonyu2.exe";
+   for my $f (@files) {
+      my @path=split "/", $f;
+      pop @path;
+      $path=join "/", @path;
+      &system2( "cp $src/$f $dst/$path ");
+   }
+}
+sub copyNonDebug {
+   my $dst="Runtimes";
+   my $src=$nonDebugDir;
+   my @files=@baseFiles;
+   push @files, "Player2.exe";
+   for my $f (@files) {
+      my @path=split "/", $f;
+      pop @path;
+      $path=join "/", @path;
+      &system2( "cp $src/$f $dst/$path ");
+   }
+}
+sub system2 {
+   print $_[0]."\n";
+}
