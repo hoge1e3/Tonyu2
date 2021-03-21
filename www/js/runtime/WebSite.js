811c694fffa4b5b2d1316666d99aca651a9fb26c
hoge1e3 <hoge1e3@gmail.com>
2021-03-21 19:22:24 +0900

    build

--- www/js/runtime/WebSite.js
+++ www/js/runtime/WebSite.js
@@ -8,7 +8,7 @@ define(["FS","Platform","root"], function (FS,Platform,root) {
 	var prot=location.protocol;
 	var k;
 
-	var VER=1608448206499;
+	var VER=1616322060404;
 	if (!prot.match(/^http/)) prot="https:";
 	switch(WebSite.runType) {
 	case "IDE":
