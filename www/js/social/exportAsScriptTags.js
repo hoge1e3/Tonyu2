676a6157298582e035f93fbc84a222af56b13e55
hoge1e3 <hoge1e3@gmail.com>
2022-11-20 21:29:28 +0900

    HTMLに出力したとき，options.jsonが重複する不具合を修正

--- www/js/social/exportAsScriptTags.js
+++ www/js/social/exportAsScriptTags.js
@@ -31,18 +31,18 @@ define(["FS","Util","WebSite","splashElement"], function (FS,Util,WebSite,splash
             buf+='<script src="'+genPath+'runScript_concat.min.js" type="text/javascript"></script>\n';
             buf+=`<script src="${scriptServer}js/runtime/detectUnsupported.js"></script>`;
         }
-        var binary=[],json=[];
+        var binary=[],json=[],visited={};
         //dir=FS.get(dir);
         dir.recursive(function (f) {
             var rel=f.relPath(dir);
             if (excludes[rel]) return;
-            if (f.endsWith(".json") && rel.indexOf("maps/")<0) {
+            if (rel==="options.json") {
+                return;
+            } else if (f.endsWith(".json") && rel.indexOf("maps/")<0) {
                 json.push(f);
                 return;
             } else if (rel.indexOf("static/")>=0) {
                 binary.push(f);
-                return;
-            } else if (rel===("options.json")) {
                 return;
             } else if (f.endsWith(".desktop")) {
                 return;
@@ -59,6 +59,7 @@ define(["FS","Util","WebSite","splashElement"], function (FS,Util,WebSite,splash
             //var name=f.truncExt(".tonyu");
             //var m="";//(name==main?" data-main='true'":"");
             var lu=" data-lastupdate='"+f.lastUpdate()+"' ";
+            if (chkDup(rel,"base")) return;
             buf+="<script language='text/tonyu' type='text/tonyu' data-filename='"+rel+"'"+lu+">";
             buf+=escapeLoosely(f.text());
             buf+="</script>\n\n";
@@ -66,18 +67,22 @@ define(["FS","Util","WebSite","splashElement"], function (FS,Util,WebSite,splash
         json.forEach(function (f) {
             var rel=f.relPath(dir);
             var lu=" data-lastupdate='"+f.lastUpdate()+"' ";
+            if (chkDup(rel,"json")) return;
             buf+="<script language='text/tonyu' type='text/tonyu' data-filename='"+rel+"'"+lu+">\n";
             buf+=beautifyJSON(f.text());
             buf+="</script>\n\n";
         });
         let lu=` data-lastupdate='${new Date().getTime()}' `;
-        buf+="<script language='text/tonyu' type='text/tonyu' data-filename='options.json'"+lu+">\n";
-        buf+=JSON.stringify(popt,null,4);;
-        buf+="</script>\n\n";
+        if (!chkDup("options.json","options")) { 
+            buf+="<script language='text/tonyu' type='text/tonyu' data-filename='options.json'"+lu+">\n";
+            buf+=JSON.stringify(popt,null,4);
+            buf+="</script>\n\n";
+        }
         for (let ns of Object.keys(deps)) {
             const f=deps[ns].srcFile;
             let lu=" data-lastupdate='"+f.lastUpdate()+"' ";
             let rel=deps[ns].dstPath;
+            if (chkDup(rel,"deps")) continue;
             buf+="<script language='text/tonyu' type='text/tonyu' data-filename='"+rel+"' data-wrap='80'"+lu+">";
             buf+=escapeLoosely(wrap(f.text(),80));
             buf+="</script>\n\n";
@@ -85,12 +90,21 @@ define(["FS","Util","WebSite","splashElement"], function (FS,Util,WebSite,splash
         binary.forEach(function (f) {
             var rel=f.relPath(dir);
             var lu=" data-lastupdate='"+f.lastUpdate()+"' ";
+            if (chkDup(rel,"binary")) return;
             buf+="<script language='text/tonyu' type='text/tonyu' data-filename='"+rel+"' data-wrap='80'"+lu+">";
             buf+=wrap(f.text(),80);
             buf+="</script>\n\n";
         });
         buf+="</head><body>"+(options.splashElement||splashElement)+"</body></html>";
         return buf;
+        function chkDup(rel,label) {
+            if (visited[rel]) {
+                console.log(rel, "Already in ",visited[rel]);
+                return true;
+            }
+            visited[rel]=label;
+            return false;
+        }
         function wrap(str, cols) {
             var lines=str.split("\n");
             var buf="";
