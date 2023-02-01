d8d0eb48afbadc4cc4b4d0a16576bc325e6c361b
hoge1e3 <hoge1e3@gmail.com>
2023-02-01 12:04:14 +0900

    callメソッドで，呼び出した関数の戻り値がfalsyだった場合にcallの戻り値がundefinedになる不具合修正

--- www/js/runtime/TonyuRuntime.js
+++ www/js/runtime/TonyuRuntime.js
@@ -597,7 +597,7 @@ function addMeta(fn, m) {
     return extend(klass.getMeta(fn), m);
 }
 function getMeta(klass) {
-    if (RuntimeTypes_1.isTonyuClass(klass))
+    if ((0, RuntimeTypes_1.isTonyuClass)(klass))
         return klass.meta;
     return klass;
 }
@@ -788,7 +788,7 @@ var klass = {
             prot.getClassInfo = function () {
                 return res.meta;
             };
-            if (RuntimeTypes_1.isTonyuClass(res))
+            if ((0, RuntimeTypes_1.isTonyuClass)(res))
                 chkclass(res);
             return res; //chkclass(res,{isShim, init:false, includesRec:{}});
         }
@@ -877,7 +877,7 @@ function getClass(n) {
                     found = nn + "." + n;
                 }
                 else
-                    throw new Error(R_1.default("ambiguousClassName", nn, n, found));
+                    throw new Error((0, R_1.default)("ambiguousClassName", nn, n, found));
             }
         }
     }
@@ -902,20 +902,20 @@ function bindFunc(t, meth) {
 }
 function invokeMethod(t, name, args, objName) {
     if (!t)
-        throw new Error(R_1.default("cannotInvokeMethod", objName, t, name));
+        throw new Error((0, R_1.default)("cannotInvokeMethod", objName, t, name));
     var f = t[name];
     if (typeof f != "function")
-        throw new Error(R_1.default("notAMethod", (objName == "this" ? "" : objName + "."), name, f));
+        throw new Error((0, R_1.default)("notAMethod", (objName == "this" ? "" : objName + "."), name, f));
     return f.apply(t, args);
 }
 function callFunc(f, args, fName) {
     if (typeof f != "function")
-        throw new Error(R_1.default("notAFunction", fName));
+        throw new Error((0, R_1.default)("notAFunction", fName));
     return f.apply({}, args);
 }
 function checkNonNull(v, name) {
     if (v != v || v == null)
-        throw new Error(R_1.default("uninitialized", name, v));
+        throw new Error((0, R_1.default)("uninitialized", name, v));
     return v;
 }
 function A(args) {
@@ -926,7 +926,7 @@ function A(args) {
     return res;
 }
 function useNew(c) {
-    throw new Error(R_1.default("newIsRequiredOnInstanciate", c));
+    throw new Error((0, R_1.default)("newIsRequiredOnInstanciate", c));
 }
 function not_a_tonyu_object(o) {
     console.log("Not a tonyu object: ", o);
@@ -937,8 +937,8 @@ function hasKey(k, obj) {
 }
 function run(bootClassName) {
     var bootClass = getClass(bootClassName);
-    if (!RuntimeTypes_1.isTonyuClass(bootClass))
-        throw new Error(R_1.default("bootClassIsNotFound", bootClassName));
+    if (!(0, RuntimeTypes_1.isTonyuClass)(bootClass))
+        throw new Error((0, R_1.default)("bootClassIsNotFound", bootClassName));
     Tonyu.runMode = true;
     var boot = new bootClass();
     //var th=thread();
@@ -957,7 +957,7 @@ function checkLoop() {
     var now = root_1.default.performance.now();
     if (now - lastLoopCheck > 1000) {
         resetLoopCheck(10000);
-        throw new Error(R_1.default("infiniteLoopDetected"));
+        throw new Error((0, R_1.default)("infiniteLoopDetected"));
     }
     prevCheckLoopCalled = now;
 }
@@ -971,7 +971,7 @@ function is(obj, klass) {
         return false;
     if (obj instanceof klass)
         return true;
-    if (typeof obj.getClassInfo === "function" && RuntimeTypes_1.isTonyuClass(klass)) {
+    if (typeof obj.getClassInfo === "function" && (0, RuntimeTypes_1.isTonyuClass)(klass)) {
         return obj.getClassInfo().includesRec[klass.meta.fullName];
     }
     return false;
@@ -1101,7 +1101,7 @@ class TonyuThread {
         if (typeof methodName == "string") {
             method = obj["fiber$" + methodName];
             if (!method) {
-                throw new Error(R_1.default("undefinedMethod", methodName));
+                throw new Error((0, R_1.default)("undefinedMethod", methodName));
             }
         }
         if (typeof methodName == "function") {
@@ -1109,7 +1109,7 @@ class TonyuThread {
             method = fmethod.fiber;
             if (!method) {
                 var n = fmethod.methodInfo ? fmethod.methodInfo.name : fmethod.name;
-                throw new Error(R_1.default("notAWaitableMethod", n));
+                throw new Error((0, R_1.default)("notAWaitableMethod", n));
             }
         }
         args = [this].concat(args);
@@ -1293,13 +1293,14 @@ class TonyuThread {
         try {
             while (performance.now() < lim && !this.fSuspended) {
                 const n = this.generator.next();
-                if (n.value) {
-                    awaited = n.value;
-                    break;
-                }
                 if (n.done) {
                     this.termStatus = "success";
+                    this.retVal = n.value;
                     this.notifyEnd(this.retVal);
+                    break;
+                }
+                if (n.value) {
+                    awaited = n.value;
                     break;
                 }
             }
